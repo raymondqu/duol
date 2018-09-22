00109234b414c77557986eaa0d2a9c99d6b6aaad
bill-l1 <x711li@gmail.com>
2018-09-22 17:00:43 -0400

    Added oop and first direction-data response for touch

--- YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/MainActivity.java
+++ YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/MainActivity.java
@@ -1,58 +1,46 @@
 package com.example.aydan.yorkuhacks;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.view.MotionEvent;
+import android.content.Intent;
+import android.util.Log;
 
+public class MainActivity extends Activity{
 
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-
-
-public class MainActivity extends Activity implements SensorEventListener{
-    public int direction = 0;
-
-    private Sensor mySensor;
-    private SensorManager SM;
-
+    public Boolean sensorToggled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Create our Sensor Manager
-        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
-
-        // Accelerometer Sensor
-        mySensor = SM.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-
-        // Register sensor Listener
-        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Not in use
-    }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         int eventaction = event.getAction();
 
-        switch (eventaction) {
-            case MotionEvent.ACTION_DOWN:
+        if(!sensorToggled){
+            sensorToggled = true;
+            Intent i=new Intent(this, SensorActivity.class);
+            i.putExtra("sensorToggled", sensorToggled);
+            startActivityForResult(i, 1);
+        }
+
+        return true;
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                int direction=data.getIntExtra("result", 1);
+
                 switch(direction) {
                     case 1:
                         Log.d("MainActivity", "LEFT");
-
                         break;
                     case 2:
                         Log.d("MainActivity", "UP");
@@ -64,35 +52,14 @@ public class MainActivity extends Activity implements SensorEventListener{
                         Log.d("MainActivity", "DOWN");
                         break;
                 }
-                break;
-            default:
-                break;
+                sensorToggled = false;
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }//onActivityResult
 
-        }
-        return true;
-
-
-    }
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if(event.values[0] < -2)
-        {
-            direction = 1;
-        }
-        if(event.values[2] > 2)
-        {
-            direction = 2;
-        }
-        if(event.values[0] > 2)
-        {
-            direction = 3;
-        }
-        if(event.values[2] < -2)
-        {
-            direction = 4;
-        }
-
-    }
     //commited at 3:46
 
 
---
+++ YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/SensorActivity.java
@@ -0,0 +1,77 @@
+package com.example.aydan.yorkuhacks;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+import android.content.Intent;
+
+public class SensorActivity extends Activity implements SensorEventListener{
+    public int direction = 0;
+
+    private Sensor mySensor;
+    private SensorManager SM;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create our Sensor Manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        // Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        // Register sensor Listener
+        super.onResume();
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not in use
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.values[0] < -2)
+        {
+            sendDirection(1);
+        }
+        if(event.values[2] > 2)
+        {
+            sendDirection(2);
+        }
+        if(event.values[0] > 2)
+        {
+            sendDirection(3);
+        }
+        if(event.values[2] < -2)
+        {
+            sendDirection(4);
+        }
+
+        //Log.d("y:", Float.toString(event.values[1]));
+    }
+    //commited at 3:46
+
+    public void sendDirection(int direction){
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("result", direction);
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
+
+        super.onPause();
+        SM.unregisterListener(this);
+    }
+
+}
