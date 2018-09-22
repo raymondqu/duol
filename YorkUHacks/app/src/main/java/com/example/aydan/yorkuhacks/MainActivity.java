325d3217c59d1fb3eb7c341c437675cb4359f3ad
helpmynamewontfi <aydan.jiwani@gmail.com>
2018-09-22 16:58:34 -0400

    wifi shit yeet

--- YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/MainActivity.java
+++ YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/MainActivity.java
@@ -1,22 +1,66 @@
 package com.example.aydan.yorkuhacks;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-//import android.view.Menu;
-//import android.view.MenuItem;
+import android.view.View;
+import android.view.MotionEvent;
+
 
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-//import android.widget.TextView;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate.Status;
+import com.google.android.gms.nearby.connection.Strategy;
+
 
 public class MainActivity extends Activity implements SensorEventListener{
-
+    public int direction = 0;
 
     private Sensor mySensor;
     private SensorManager SM;
+
+    private static final String[] REQUIRED_PERMISSIONS =
+            new String[] {
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            };
+
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +71,7 @@ public class MainActivity extends Activity implements SensorEventListener{
         SM = (SensorManager)getSystemService(SENSOR_SERVICE);
 
         // Accelerometer Sensor
-        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
         // Register sensor Listener
         SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
@@ -39,11 +83,58 @@ public class MainActivity extends Activity implements SensorEventListener{
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Not in use
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int eventaction = event.getAction();
 
+        switch (eventaction) {
+            case MotionEvent.ACTION_DOWN:
+                switch(direction) {
+                    case 1:
+                        Log.d("MainActivity", "LEFT");
+
+                        break;
+                    case 2:
+                        Log.d("MainActivity", "UP");
+                        break;
+                    case 3:
+                        Log.d("MainActivity", "RIGHT");
+                        break;
+                    case 4:
+                        Log.d("MainActivity", "DOWN");
+                        break;
+                }
+                break;
+            default:
+                break;
+
+        }
+        return true;
+
+
+    }
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.d("MainActivity", Float.toString(event.values[0]) + Float.toString(event.values[1])+ Float.toString(event.values[2]));
+        if(event.values[0] < -4)
+        {
+            direction = 1;
+        }
+        if(event.values[2] > 4)
+        {
+            direction = 2;
+        }
+        if(event.values[0] > 4)
+        {
+            direction = 3;
+        }
+        if(event.values[2] < -4)
+        {
+            direction = 4;
+        }
+
     }
+    //commited at 3:46
+
 
 
 }
