a0bd02498846171dede7ab84ae1c94629e37f775
helpmynamewontfi <aydan.jiwani@gmail.com>
2018-09-22 15:46:57 -0400

    direction input working

--- YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/MainActivity.java
+++ YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.MotionEvent;
+
 
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
@@ -13,10 +16,11 @@ import android.hardware.SensorEventListener;
 
 
 public class MainActivity extends Activity implements SensorEventListener{
-
+    public int direction = 0;
 
     private Sensor mySensor;
     private SensorManager SM;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +43,57 @@ public class MainActivity extends Activity implements SensorEventListener{
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Not in use
     }
-
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        Log.d("MainActivity", "x: " + Float.toString(event.values[0]) +"y: " + Float.toString(event.values[1]) + "z: " + Float.toString(event.values[2]));
+    public boolean onTouchEvent(MotionEvent event) {
+        int eventaction = event.getAction();
+
+        switch (eventaction) {
+            case MotionEvent.ACTION_DOWN:
+                switch(direction) {
+                    case 1:
+                        Log.d("MainActivity", "LEFT");
+
+                        break;
+                    case 2:
+                        Log.d("MainActivity", "UP");
+                        break;
+                    case 3:
+                        Log.d("MainActivity", "RIGHT");
+                        break;
+                    case 4:
+                        Log.d("MainActivity", "DOWN");
+                        break;
+                }
+                break;
+            default:
+                break;
+
+        }
+        return true;
+
 
     }
-    //daberino
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.values[0] < -2)
+        {
+            direction = 1;
+        }
+        if(event.values[2] > 2)
+        {
+            direction = 2;
+        }
+        if(event.values[0] > 2)
+        {
+            direction = 3;
+        }
+        if(event.values[2] < -2)
+        {
+            direction = 4;
+        }
 
+    }
+    //commited at 3:46
 
 
 
