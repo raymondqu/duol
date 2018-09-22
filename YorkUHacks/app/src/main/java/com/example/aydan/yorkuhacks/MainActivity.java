c764ed77796e19825332f6a324de1bed30ba2fb6
helpmynamewontfi <aydan.jiwani@gmail.com>
2018-09-22 16:57:58 -0400

    nearby communication
    in progress

--- YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/MainActivity.java
+++ YorkUHacks/app/src/main/java/com/example/aydan/yorkuhacks/MainActivity.java
@@ -15,12 +15,52 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate.Status;
+import com.google.android.gms.nearby.connection.Strategy;
+
+
 public class MainActivity extends Activity implements SensorEventListener{
     public int direction = 0;
 
     private Sensor mySensor;
     private SensorManager SM;
 
+    private static final String[] REQUIRED_PERMISSIONS =
+            new String[] {
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            };
+
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,19 +115,19 @@ public class MainActivity extends Activity implements SensorEventListener{
     }
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if(event.values[0] < -2)
+        if(event.values[0] < -4)
         {
             direction = 1;
         }
-        if(event.values[2] > 2)
+        if(event.values[2] > 4)
         {
             direction = 2;
         }
-        if(event.values[0] > 2)
+        if(event.values[0] > 4)
         {
             direction = 3;
         }
-        if(event.values[2] < -2)
+        if(event.values[2] < -4)
         {
             direction = 4;
         }
